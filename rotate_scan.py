afd54828f6ae04b190439b1121e91a72bf1bf8fd
handh0 <handh0@yonsei.ac.kr>
2023-06-28 18:15:54 +0900

    주석 수정

--- rotate_scan.py
+++ rotate_scan.py
@@ -10,7 +10,7 @@ import time
 import pyautogui
 import threading
 import sys
-import win32gui
+import win32gui #창 열람 방식이 바뀌어서 사용하지 않습니다. 이후 코드를 정리하면서 삭제할 예정입니다.
 
 pyautogui.FAILSAFE = False
  
@@ -21,14 +21,15 @@ click_type = 1 # 1은 단일 클릭, 2은 더블 클릭
 
 
 key_state = False # 키보드 입력 확인
-change_signal = False # move event 중 change 시 rapaint 호출 위함
+change_signal = False # move event 중 change 시 rapaint 호출 위함. 입력 방식 변경 상태 저장
 
+#반지름 선의 시작점과 끝점 좌표
 start_x = 0
 start_y = 0
 end_x = 0
 end_y = 0
 
-settings = QSettings("config.ini", QSettings.IniFormat)
+settings = QSettings("config.ini", QSettings.IniFormat) #설정파일 값 조회
 
 #키 입력용 쓰레드
 class key_listener(threading.Thread):
@@ -36,10 +37,12 @@ class key_listener(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         
-        self.rep_check = False
+        self.rep_check = False #반복(키를 누르고 있을 때 여러번 입력이 들어가는 것) 방지.
+
         # 0은 누를 때, 1은 땔 때
         self.click_state = int(settings.value("SPIN/click_state"))
 
+        #키 초기화. 
         self.target_key = getattr(keyboard.Key, settings.value("SPIN/target_key"))
         self.change_key = getattr(keyboard.Key, settings.value("SPIN/change_key"))
         self.end_key = getattr(keyboard.Key, settings.value("SPIN/end_key"))
@@ -49,14 +52,14 @@ class key_listener(threading.Thread):
         global click_type
         global change_signal
         
-        if self.click_state == 0:
-            if self.rep_check == False:
-                if key == self.target_key:
-                    key_state = True
-                    self.rep_check = True
+        if self.click_state == 0: #press 감지
+            if self.rep_check == False:     # 반복된 입력이 아니고
+                if key == self.target_key:  # 지정한 키 입력일 때
+                    key_state = True        # 입력 상태로 전환하고
+                    self.rep_check = True   # 반복 방지
                     
                 elif key == self.change_key:
-                    click_type = (click_type % 2) + 1
+                    click_type = (click_type % 2) + 1 # 0은 1로, 1은 0으로 전환
                     change_signal = True
                     self.rep_check = True
 
@@ -67,17 +70,22 @@ class key_listener(threading.Thread):
         global click_type
         global change_signal
 
-        if key == self.end_key:
+        if key == self.end_key: # 보호자 프로그램 종료 키. release 일 때만 적용
             # Stop listener
             return False
 
         if self.click_state == 1:
-            if key == self.target_key:
+            if key == self.target_key: # release는 반복 문제가 없기 때문에 지정키 대응만 점검
                 key_state = True
             elif key == self.change_key:
                 click_type = (click_type % 2) + 1
                 change_signal = True
                 
+        '''
+        rep_check를 True로 만든 키인지 검사하는 코드가 없기 때문에
+        a를 press한 상태에서 b를 press-release 했을 때 반복 입력이 됨.
+        사용 환경이 별도의 키를 이용한다는 가정 하에 문제가 없다고 판단하여 제외함
+        '''
         self.rep_check = False
         
 
@@ -88,11 +96,11 @@ class key_listener(threading.Thread):
             on_release=self.on_release) as listener:
             
             listener.join()
-            
+        # keyboard Listener가 종료되었을 때: 프로그램 종료
         QCoreApplication.instance().quit()
 
 
-def click(x_pos, y_pos):
+def click(x_pos, y_pos):    # 지정 좌표에 클릭 수행
     pyautogui.moveTo(x_pos, y_pos, _pause = False)
     if click_type == 1:
         pyautogui.click()
@@ -118,6 +126,7 @@ class DisplayWindow(QMainWindow):
         win_control.start()
         #thread 실행
 
+        # 설정 값 초기화
         r = int(settings.value("SPIN/line_color_r"))
         g = int(settings.value("SPIN/line_color_g"))
         b = int(settings.value("SPIN/line_color_b"))
@@ -156,7 +165,7 @@ def accurate_delay(delay):
     while time.perf_counter() < t:
         pass
 
-def windowEnumerationHandler(hwnd, top_windows):
+def windowEnumerationHandler(hwnd, top_windows):#미사용
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
 def program_to_front():#미사용
@@ -172,6 +181,7 @@ def program_to_front():#미사용
             except:
                 break
 
+# paint 값과 타이밍을 조절하는 컨트롤러 쓰레드
 class WindowController(QThread):
     global key_state
     
@@ -179,27 +189,30 @@ class WindowController(QThread):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        # 추가 회전 시간
+        # 추가 회전 시간 (기존 paint 함수 딜레이 + rotate_wait)
         self.rotate_wait = int(settings.value("SPIN/rotate_wait"))
         self.circle_point = generate_circle_coordinate()
 
-        self.move_wait = int(settings.value("SPIN/move_wait"))
+        self.move_wait = int(settings.value("SPIN/move_wait")) # 마우스 커서 움직임 대기시간
 
+        #제한 횟수: 이전 단계로 돌아가는 기준
         self.rotate_time = int(settings.value("SPIN/rotate_time"))
         self.move_time = int(settings.value("SPIN/move_time"))
 
+        #시작 전 대기시간
         self.rotate_delay = int(settings.value("SPIN/rotate_delay"))
         self.move_delay = int(settings.value("SPIN/move_delay"))
-
+        
+        #동작 복귀를 위한 상태 값(0: 초기 1: 회전)
         self.current_state = 0
 
-    def key_wait(self):
+    def key_wait(self): # 키 입력 대기. 
         while key_state == False:
             accurate_delay(0.1)
             pass
         return
 
-    def spin_line(self):
+    def spin_line(self): # 좌표를 만들어서 키 입력때까지 회전함
         global end_x, end_y
 
         for x, y in self.circle_point:  
@@ -220,13 +233,13 @@ class WindowController(QThread):
         end_x = 0
         end_y = 0
 
-    def run(self):
+    def run(self): #전체 동작 관리
         global key_state, start_x, start_y, end_x, end_y
         global change_signal
         while True:
             # program_to_front()
             count = 0
-            if self.current_state == 0:
+            if self.current_state == 0: #초기 상태일 때
                 self.init_point()
                 self.current_state = 1
 
